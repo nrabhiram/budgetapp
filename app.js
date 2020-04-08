e621fd48754e4a836700d82a2f7ee0301d7d98cc
Abhiram Reddy <62073792+nrabhiram@users.noreply.github.com>
2020-04-08 20:44:46 +0530

    Prerequisites
    Added the index and style sheets.

---
+++ app.js
@@ -0,0 +1,11 @@
+var budgetController = (function() {
+
+})();
+
+var UIController = (function() {
+
+})();
+
+var Controller = (function(budgetCtrl, UICtrl) {
+
+})(budgetController, UIController);
